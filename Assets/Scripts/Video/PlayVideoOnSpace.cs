acd0675284fe17461042ae154c0c7154de402129
jpmmaia <jpmmaia@gmail.com>
2017-02-22 12:39:42 +0000

    - Initializing project. - Setting up a MovieTexture.

---
+++ Assets/Scripts/Video/PlayVideoOnSpace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideoOnSpace : MonoBehaviour
+{
+    public MovieTexture movieTexture;
+
+    void Update()
+    {
+        if (!Input.GetButtonDown("Jump")) return;
+
+        if (movieTexture.isPlaying)
+        {
+            movieTexture.Pause();
+        }
+        else
+        {
+            movieTexture.Play();
+        }
+    }
+}
+
