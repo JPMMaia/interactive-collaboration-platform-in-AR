a15a5551291293de0fed346acf4d93685f4c9341
jpmmaia <jpmmaia@gmail.com>
2017-03-14 17:53:22 +0000

    - Adding kudan.

---
+++ Client/Assets/Scripts/Kudan/KudanEventsHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kudan.AR;
+
+public class KudanEventsHandler : MonoBehaviour
+{
+    public string TextToShow = string.Empty;
+    public GameObject PrefabToInstantiate;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(400, 400, 300, 100), TextToShow);
+    }
+
+    public void FoundNewMarker(Trackable marker)
+    {
+        TextToShow = "Found new marker";
+    }
+
+    public void FoundOldMarker(Trackable marker)
+    {
+        TextToShow = "Found old marker";
+    }
+
+    public void LostMarker(Trackable marker)
+    {
+        TextToShow = "Lost marker";
+    }
+}
---
+++ Client/Assets/Scripts/Kudan/PlaceMarkerlessObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Kudan.AR;
+
+namespace Assets.Scripts.Assets.Scripts.Kudan
+{
+    public class PlaceMarkerlessObject : MonoBehaviour
+    {
+        public KudanTracker _kudanTracker;
+
+        public void PlaceClick()
+        {
+            Vector3 position;
+            Quaternion orientation;
+
+            _kudanTracker.FloorPlaceGetPose(out position, out orientation);
+            _kudanTracker.ArbiTrackStart(position, orientation);
+        }
+    }
+}
