956b6d4d443a2a40247c52329314f97b1e386abe
jpmmaia <jpmmaia@gmail.com>
2017-03-15 02:50:21 +0000

    - Completing AR - network prototype.

---
+++ Client/Assets/Scripts/Client/ClientController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Assets.Scripts.Client
+{
+    public class ClientController : MonoBehaviour
+    {
+        public Camera Camera;
+        public int SceneLayer = 256;
+
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+                ToggleSceneVisibility();
+        }
+
+        public void ToggleSceneVisibility()
+        {
+            Camera.cullingMask ^= SceneLayer;
+        }
+    }
+}
---
+++ Client/Assets/Scripts/Server/ServerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using Assets.Scripts.Scenes;
+
+namespace Assets.Scripts.Assets.Scripts.Server
+{
+    public class ServerController : MonoBehaviour
+    {
+        public Camera MainCamera;
+        public Scene Scene;
+
+        public GameObject ArrowPrefab;
+        public GameObject RubiksPrefab;
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                var ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+                if(Physics.Raycast(ray, out hitInfo))
+                {
+                    var worldPosition = hitInfo.point;
+                    var worldToLocalMatrix = Scene.transform.worldToLocalMatrix;
+                    var localPosition = worldToLocalMatrix.MultiplyPoint(worldPosition);
+
+                    Scene.CmdAdd(ArrowPrefab, localPosition, Quaternion.identity, 1, false);
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Scene.CmdAdd(RubiksPrefab, Vector3.zero, Quaternion.identity, 8, false);
+            }
+        }
+    }
+}
